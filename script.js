86d32cc76d8f81c09dbc2971c6451d285741ede7
hannvtwu <sonyjihanrianto@gmail.com>
2024-10-19 22:16:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll to sections smoothly when clicking nav links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
